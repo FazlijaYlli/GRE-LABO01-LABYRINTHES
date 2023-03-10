dd7efa73a09d46b6fa7a29151372c7859aaddcd0
Rui Carneiro <rui.motacarneiro@heig-vd.ch>
2023-03-10 16:31:29 +0100

    Non-static methods of GridGraph

--- src/main/java/gre/lab1/groupe12/GridGraph.java
+++ src/main/java/gre/lab1/groupe12/GridGraph.java
@@ -2,87 +2,115 @@ package gre.lab1.groupe12;
 
 import gre.lab1.graph.GridGraph2D;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 // TODO: javadoc
 public final class GridGraph implements GridGraph2D {
-  /** Largeur de la grille. */
-  private final int width;
+    /**
+     * Largeur de la grille.
+     */
+    private final int width;
 
-  /** Hauteur de la grille. */
-  private final int height;
+    /**
+     * Hauteur de la grille.
+     */
+    private final int height;
 
-  /**
-   * Construit une grille carrée.
-   * @param side Côté de la grille.
-   */
-  public GridGraph(int side) {
-    this(side, side);
-  }
+    /**
+     * Listes d'adjacence
+     */
+    ArrayList<LinkedList<Integer>> neighbors;
 
-  /**
-   * Construit une grille rectangulaire.
-   * @param width Largeur de la grille.
-   * @param height Hauteur de la grille.
-   * @throws IllegalArgumentException si {@code width} ou {@code length} sont négatifs ou nuls.
-   */
-  public GridGraph(int width, int height) {
-    if (width <= 0 || height <= 0)
-      throw new IllegalArgumentException("Width: " + width + " and height: " + height + " must be positive");
+    /**
+     * Construit une grille carrée.
+     *
+     * @param side Côté de la grille.
+     */
+    public GridGraph(int side) {
+        this(side, side);
+    }
 
-    this.width = width;
-    this.height = height;
-  }
+    /**
+     * Construit une grille rectangulaire.
+     *
+     * @param width  Largeur de la grille.
+     * @param height Hauteur de la grille.
+     * @throws IllegalArgumentException si {@code width} ou {@code length} sont négatifs ou nuls.
+     */
+    public GridGraph(int width, int height) {
+        if (width <= 0 || height <= 0)
+            throw new IllegalArgumentException("Width: " + width + " and height: " + height + " must be positive");
 
-  @Override
-  public List<Integer> neighbors(int v) {
-    // TODO: A implémenter
-    return null;
-  }
+        this.width = width;
+        this.height = height;
 
-  @Override
-  public boolean areAdjacent(int u, int v) {
-    // TODO: A implémenter
-    return false;
-  }
+        neighbors = new ArrayList<>(width * height);
+    }
 
-  @Override
-  public void addEdge(int u, int v) {
-    // TODO: A implémenter
-  }
+    @Override
+    public List<Integer> neighbors(int v) {
 
-  @Override
-  public void removeEdge(int u, int v) {
-    // TODO: A implémenter
-  }
+        return new ArrayList<>(neighbors.get(v));
+    }
 
-  @Override
-  public int nbVertices() {
-    // TODO: A implémenter
-    return 0;
-  }
+    @Override
+    public boolean areAdjacent(int u, int v) {
 
-  @Override
-  public boolean vertexExists(int v) {
-    // TODO: A implémenter
-    return false;
-  }
+        if (neighbors.get(u).isEmpty()) return false;
 
-  @Override
-  public int width() {
-    return width;
-  }
+        for (int x : neighbors.get(u)) {
+            if (x == v) return true;
+        }
 
-  @Override
-  public int height() {
-    return height;
-  }
+        return false;
+    }
 
-  /**
-   * Lie chaque sommet du graphe donné à tous ses voisins dans la grille.
-   * @param graph Un graphe.
-   */
-  public static void bindAll(GridGraph graph) {
-    // TODO: A implémenter
-  }
+    @Override
+    public void addEdge(int u, int v) {
+
+        if (!neighbors.get(u).contains(v)) {
+            neighbors.get(u).add(v);
+            neighbors.get(v).add(u);
+        }
+    }
+
+    @Override
+    public void removeEdge(int u, int v) {
+
+        if (neighbors.get(u).contains(v)) {
+            neighbors.get(u).removeFirstOccurrence(v);
+            neighbors.get(v).removeFirstOccurrence(u);
+        }
+    }
+
+    @Override
+    public int nbVertices() {
+        return neighbors.size();
+    }
+
+    @Override
+    public boolean vertexExists(int v) {
+        return v < neighbors.size();
+    }
+
+    @Override
+    public int width() {
+        return width;
+    }
+
+    @Override
+    public int height() {
+        return height;
+    }
+
+    /**
+     * Lie chaque sommet du graphe donné à tous ses voisins dans la grille.
+     *
+     * @param graph Un graphe.
+     */
+    public static void bindAll(GridGraph graph) {
+        // TODO: A implémenter
+    }
 }
